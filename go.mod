2c9eeebb767134d3d05ffecef47d42555bdc8f6a
SuLinXin66 <15210204903@163.com>
2022-10-07 18:50:48 +0800

    增加验证请求签名的中间件

--- go.mod
+++ go.mod
@@ -5,5 +5,12 @@ go 1.19
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-base-lib/logs v0.0.0-20220723204936-3aac9518a91e
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+)
+
+require (
+	github.com/go-base-lib/coderutils v0.0.0-20221003111253-ea24e02dc926 // indirect
+	github.com/go-base-lib/goextension v0.0.0-20221003104525-59fbe063e6ad // indirect
+	github.com/tjfoc/gmsm v1.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
 )
--- useragent.go
+++ useragent.go
@@ -1,9 +1,26 @@
 package ginmiddleware
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
 	"github.com/gin-gonic/gin"
+	"github.com/go-base-lib/coderutils"
+	"github.com/go-base-lib/goextension"
+	"github.com/tjfoc/gmsm/sm2"
 	"net/http"
 )
+
+func UserVerifyHeaderWithVerifyFn(fn func(header http.Header) bool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if !fn(ctx.Request.Header) {
+			ctx.Abort()
+			ctx.Status(http.StatusMethodNotAllowed)
+			return
+		}
+		ctx.Next()
+	}
+}
 
 func UseVerifyUserAgent(targetUserAgent string) gin.HandlerFunc {
 	return UseVerifyUserAgentWithVerifyFn(func(userAgent string) bool {
@@ -12,13 +29,78 @@ func UseVerifyUserAgent(targetUserAgent string) gin.HandlerFunc {
 }
 
 func UseVerifyUserAgentWithVerifyFn(fn func(userAgent string) bool) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		userAgent := ctx.GetHeader("User-Agent")
-		if !fn(userAgent) {
-			ctx.Abort()
-			ctx.Status(http.StatusMethodNotAllowed)
+	return UserVerifyHeaderWithVerifyFn(func(header http.Header) bool {
+		return fn(header.Get("User-Agent"))
+	})
+}
+
+func DefaultOriginDataGet(ctx *gin.Context) []byte {
+	return []byte(ctx.Request.RequestURI)
+}
+
+func DefaultRsaOriginDataGet(ctx *gin.Context) []byte {
+	h, _ := coderutils.Hash(sha256.New(), []byte(ctx.Request.RequestURI))
+	return h
+}
+
+func DefaultDigestGet(ctx *gin.Context) []byte {
+	digest, _ := goextension.Bytes(ctx.GetHeader("_d")).DecodeBase64()
+	return digest
+}
+
+type VerifySignRequestOption struct {
+	// RSAPublicKey rsa公钥
+	RSAPublicKey *rsa.PublicKey
+	// SM2PublicKey Sm2公钥
+	SM2PublicKey *sm2.PublicKey
+	// OriginGet 原文获取， 默认使用 DefaultOriginDataGet, RSA默认使用 DefaultRsaOriginDataGet
+	OriginGet func(ctx *gin.Context) []byte
+	// DigestGet 签名结果获取, 默认使用 DefaultDigestGet
+	DigestGet func(ctx *gin.Context) []byte
+	// Hash hash算法获取, 默认使用 crypto.SHA256
+	Hash crypto.Hash
+}
+
+func UseVerifySignRequestPath(option *VerifySignRequestOption) gin.HandlerFunc {
+	if option != nil {
+		if option.OriginGet == nil {
+			if option.SM2PublicKey == nil && option.RSAPublicKey != nil {
+				option.OriginGet = DefaultRsaOriginDataGet
+			} else {
+				option.OriginGet = DefaultOriginDataGet
+			}
+		}
+
+		if option.DigestGet == nil {
+			option.DigestGet = DefaultDigestGet
+		}
+
+		if option.Hash == 0 {
+			option.Hash = crypto.SHA256
+		}
+	}
+	return func(context *gin.Context) {
+		if option == nil || (option.RSAPublicKey == nil && option.SM2PublicKey == nil) {
+			context.Status(http.StatusBadRequest)
+			context.Abort()
 			return
 		}
-		ctx.Next()
+
+		origin := option.OriginGet(context)
+		digest := option.DigestGet(context)
+
+		verifyResult := false
+		if option.SM2PublicKey != nil {
+			verifyResult = option.SM2PublicKey.Verify(origin, digest)
+		} else {
+			verifyResult = rsa.VerifyPKCS1v15(option.RSAPublicKey, option.Hash, origin, digest) == nil
+		}
+
+		if !verifyResult {
+			context.Status(http.StatusMethodNotAllowed)
+			context.Abort()
+			return
+		}
+		return
 	}
 }
